5ef5b4192e9b04b7c10485eab594b9d3e9564364
李赛 <lisai@oneapm.com>
2018-01-04 16:17:46 +0800

    remove ivar defined in AppDelegate.h

--- Using Core Data/AppDelegate/AppDelegate.h
+++ Using Core Data/AppDelegate/AppDelegate.h
@@ -12,12 +12,6 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
-@property (readonly, strong, nonatomic) NSManagedObjectContext *mainMOC;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-@property (readonly, strong, nonatomic) NSManagedObjectContext *backgroundMOC;
-
 @property (strong, nonatomic) UINavigationController *navigationController;
 @property (strong, nonatomic) VocabulariesViewController *vocabulariesViewController;
 
